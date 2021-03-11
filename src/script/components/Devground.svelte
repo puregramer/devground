6e3c0781b791e9f20db7e28abbdbf295aa9e1773
charlie <charlie@millie.town>
2021-03-12 02:01:52 +0900

    - material ui

--- src/script/components/Devground.svelte
+++ src/script/components/Devground.svelte
@@ -1,5 +1,8 @@
 <script>
     import {onMount} from 'svelte';
+    import { MaterialApp, AppBar, Button, Icon } from 'svelte-materialify';
+    import { mdiMenu } from '@mdi/js';
+    let theme = 'dark';
 
     onMount(() => {
 
@@ -9,15 +12,19 @@
 </script>
 
 
-<section>
-    <div class="title">Devground</div>
-</section>>
+<MaterialApp {theme}>
+    <AppBar flat="true">
+        <div slot="icon">
+            <Button fab depressed>
+                <Icon path={mdiMenu} />
+            </Button>
+        </div>
+        <span slot="title">Devground</span>
+    </AppBar>
+</MaterialApp>
 
 
 <style type="text/scss">
-.title {
-  color: #fff;
-}
 
 
 </style>
