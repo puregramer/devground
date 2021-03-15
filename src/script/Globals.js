065b48c9e869ec3469eacf5964f015ff64c177a9
charlie-millie <charlie@millie.town>
2021-03-15 17:32:48 +0900

    - add reference

--- src/script/Globals.js
+++ src/script/Globals.js
@@ -1,3 +1,15 @@
 /*
     create by Charlie 20210309
 */
+
+/*
+- https://codepen.io/andreruffert/pen/pvqly : canvas scratch example
+- https://github.com/golovdinov/canvas-scratch : canvas scratch library
+
+- https://fusioncharts.github.io/svelte-fusioncharts/ : svelte chart library
+- https://codepen.io/JanIdsinga/pen/JRwmPb : 3d hover ani
+
+
+
+
+ */
